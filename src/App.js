1a4517ea80d96e69b9bbc7e2a2ca3266fd28ad70
Matheus Carvalho <matheusdocarvalho@gmail.com>
2023-12-27 12:47:47 -0400

    style: Configurando TailwindCSS

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+import MyComponent from "./Components/MyComponent/MyComponent";
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello World!</h1>
+      <MyComponent />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/MyComponent/MyComponent.jsx
@@ -0,0 +1,9 @@
+const MyComponent = () => {
+  return (
+    <div className="bg-blue-500 text-white p-4 mt-4">
+      Este é um componente com estilo Tailwind CSS!
+    </div>
+  );
+};
+
+export default MyComponent;
